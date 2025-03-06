533ba0033e02d46e857f0426ce12e726ca190b8e
Семен <semen@MacBook-Air-Semen.local>
2025-03-06 20:46:23 +0300

    add: icons and text

--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -32,17 +32,17 @@ class MyApp extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Общежитие №20',
-                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                'Краснодар, ул. Калинина, 13',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ]
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Общежитие №20',
+                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'Краснодар, ул. Калинина, 13',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ]
                         ),
                         Row(
                           children: [
@@ -52,6 +52,46 @@ class MyApp extends StatelessWidget {
                           ],
                         ),
                       ],
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.phone, color: Colors.green),
+                              onPressed: () {},
+                            ),
+                            Text('Позвонить'),
+                          ],
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.navigation, color: Colors.green),
+                              onPressed: () {},
+                            ),
+                            Text('Маршрут'),
+                          ],
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.share, color: Colors.green),
+                              onPressed: () {},
+                            ),
+                            Text('Поделиться'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитиях университета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
+                      style: TextStyle(fontSize: 16),
                     ),
                   ],
                 ),
