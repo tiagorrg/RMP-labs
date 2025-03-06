6a77464abae55ae1549545111b068b5416945050
Семен <semen@MacBook-Air-Semen.local>
2025-03-06 20:01:40 +0300

    add: title and favorite icon

--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -23,6 +23,39 @@ class MyApp extends StatelessWidget {
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Общежитие №20',
+                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                'Краснодар, ул. Калинина, 13',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ]
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.favorite, color: Colors.red),
+                            SizedBox(width: 8),
+                            Text('27'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
